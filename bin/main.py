71ec882091d60454f26d85853fabaf50e3cab111
charles <charles@cdormeval.home>
2024-10-30 21:15:52 +0100

    feat: break down code into function

--- bin/main.py
+++ bin/main.py
@@ -1,4 +1,6 @@
 import logging
+from logging import Logger
+from traceback import format_exc
 
 from search_recipe import search_recipe
 
@@ -14,9 +16,12 @@ def recipe_input() -> str:
     return desired_recipe
 
 
-def create_logger() -> None:
+def create_logger() -> Logger:
     """
     Create project logger with desired format
+
+    :return: project logger
+    :rtype: Logger
     """
     logger = logging.getLogger('project_logger')
     logger.setLevel(logging.DEBUG)
@@ -27,14 +32,19 @@ def create_logger() -> None:
 
     logger.addHandler(console_handler)
 
+    return logger
+
 
 def main():
     # Create project logger
-    create_logger()
-    # Get desired recipe from user
-    desired_recipe = recipe_input()
-    # Get recipe results
-    search_recipe(desired_recipe)
+    logger = create_logger()
+    try:
+        # Get desired recipe from user
+        desired_recipe = recipe_input()
+        # Get recipe results
+        search_recipe(desired_recipe)
+    except:
+        logger.error(format_exc())
 
 
 if __name__ == '__main__':
--- bin/search_recipe.py
+++ bin/search_recipe.py
@@ -1,9 +1,10 @@
 import re
 import logging
+from typing import List
 from requests import get
-from bs4 import BeautifulSoup
-from googlesearch import search
+from bs4 import BeautifulSoup, Tag
 from traceback import format_exc
+from urllib.parse import parse_qs
 
 from time import sleep
 
@@ -16,71 +17,79 @@ logger = logging.getLogger('project_logger')
 
 def search_recipe(dish:str) -> None:
     """
-    Search desired dish recipe on the internet
+    Search desired dish recipes on the internet
 
     :param recipe: _description_
     :type recipe: str
     """
+    # Create query string
     query = f"recette {dish}" if 'recette' not in dish else dish
 
+    # Extract search page to make it scrappable
+    search_page = extract_search_page(query)
+
+    # Récupération des composants recette avec notes
+    recipes_components_with_ratings = get_recipe_components_with_ratings(search_page)
+    
+    # Association des recettes à des liens
+    recipes = dict()
+    for preview in previews_with_rating:
+        url_preview_tag = preview.find('a', attrs={'class':'a-no-hover-decoration'})
+        url_preview = parse_qs(url_preview_tag.get('href',''))
+        if url_preview:
+            recipes[url_preview]['n_ratings'] = None
+            recipes[url_preview]['mean'] = None
+            recipes
+
+
+def extract_search_page(query:str) -> BeautifulSoup:
+    """
+    Get google search page for query
+
+    :param query: query to use in search bar
+    :type query: str
+    :return: search page
+    :rtype: BeautifulSoup
+    """
     driver = webdriver.Chrome()
+    logger.debug(f'Searching for {query} on google')
     driver.get(f"https://www.google.com/search?q={query}")
 
-    # Refuser cookies
-    button = driver.find_element(By.XPATH, '//div[text()="Tout refuser"]')
-    button.click()
+    # Refuse all cookies
+    disable_cookies_button = driver.find_element(By.XPATH, '//div[text()="Tout refuser"]')
+    disable_cookies_button.click()
 
-    # Afficher plus de recettes tant que possible
+    # Load all recipes
     try:
         while True:
             button = driver.find_element(By.XPATH, '//span[text()="Afficher plus"]')
             button.click()
     except:
-        pass
+        # Will happen when "Afficher plus" button will not be available anymore
+        logger.debug('All recipes loaded')
 
     # Récuération de la page source pour scrapping
-    page = BeautifulSoup(driver.page_source)
-
-    # Récupération des composants recette avec notes
-    recipes_previews = page.find_all('a', attrs={'class':'a-no-hover-decoration'})
-    previews_with_rating = list()
-    for preview in recipes_previews:
-        if 'html' in preview.text:
-            continue
-        if preview.find('span', attrs={'aria-label':re.compile('.*Note.*')}):
-            previews_with_rating.append(preview)
-    
-    # Association des recettes à des liens
-    
+    return BeautifulSoup(driver.page_source)
 
 
-def get_scores_for_recipes(recipes:list) -> dict:
-    scores = dict()
-    for recipe in recipes:
-        try:
-            rating = rating_scrapping(recipe)
-            scores[recipe] = rating
-        except:
-            logger.error(format_exc())
-        finally:
-            if not scores.get(recipe):
-                scores[recipe] = -1.0
-            logger.debug(f'Page {recipe} was attributed the score {scores.get(recipe)}')
-
-
-def rating_scrapping(recipe_link:str) -> float:
+def get_recipe_components_with_ratings(search_page:BeautifulSoup) -> List[Tag]:
     """
+    Extract recipe component from search page
+
+    :param search_page: search page (scrappable)
+    :type search_page: BeautifulSoup
+    :return: list of tags containing ratings (no ratings -> not usable for next steps)
+    :rtype: List[Tag]
+    """
+    recipes_components_with_ratings = list()
+    # Recipe components are instances of class a-no-hover-decoration
+    recipes_components = search_page.find_all('a', attrs={'class':'a-no-hover-decoration'})
+    if not recipes_components:
+        return recipes_components_with_ratings
     
-
-    :param recipe_link: recipe link
-    :type recipe_link: str
-    :return: rating if found, else -1.0
-    :rtype: float
-    """
+    for component in recipes_components:
+        # Ignore component containing all search page code + select those with note
+        if not 'html' in component.text and component.find('span', attrs={'aria-label':re.compile('.*Note.*')}):
+            recipes_components_with_ratings.append(component)
     
-
-
-def attribute_weight_to_score() -> float:
-    """
-    """
-    pass
+    return recipes_components_with_ratings
